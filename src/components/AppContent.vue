8c3473976f4ed7fc5d063af90b911dd0ec8a6995
fedeloxe <loxeee@gmail.com>
2023-01-15 16:29:41 +0100

    comics  with props  and load more button added

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,7 +1,12 @@
 <script>
-import comicsData from '../assets/data/dataComics.js';
+import comicsData from '../assets/data/dataComics.js'
+import ComicCard from './ComicCard.vue';
 
 export default {
+    components: {
+        ComicCard
+    },
+
     data() {
         return {
             comics: comicsData
@@ -11,13 +16,67 @@ export default {
 </script>
 
 <template>
-    <div class="content">
-
+    <div class="dark">
+        <div class="container">
+            <div class="title">
+                <h2>Comics Series </h2>
+            </div>
+        </div>
+        <div class="container">
+            <div class="comics">
+                <ComicCard v-for="(item, index) in comics" :key="index" :comic="item" />
+            </div>
+        </div>
+        <div class="button">
+            <a href="/">LOAD MORE</a>
+        </div>
     </div>
-
 </template>
 
 
-<style>
-.content {}
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as*;
+@use '../styles/partials/mixins' as*;
+
+.dark {
+    background-color: rgb(19, 24, 24);
+}
+
+.title {
+    position: relative;
+
+
+    h2 {
+        background-color: $blue;
+        width: 250px;
+        padding: 10px;
+        text-align: center;
+        position: absolute;
+        top: -20px;
+        color: $white;
+    }
+}
+
+.comics {
+    @include center;
+    justify-content: space-between;
+    padding-top: 100px;
+}
+
+.button {
+    padding: 20px 0;
+    display: flex;
+    justify-content: center;
+
+
+    a {
+        background-color: $blue;
+        padding: 10px;
+        text-align: center;
+        color: $white;
+        text-decoration: none;
+        margin-bottom: 20px;
+    }
+
+}
 </style>
---
+++ src/components/ComicCard.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    props: {
+        comic: Object
+    }
+
+}
+</script>
+<template>
+    <div class="comics-card">
+        <div class="thumb">
+            <img :src="comic.thumb" :alt="comic.series">
+        </div>
+        <div class="comic-title">
+            {{ comic.series }}
+        </div>
+    </div>
+
+</template>
+
+
+<style lang="scss">
+@use '../styles/partials/variables' as*;
+@use '../styles/partials/mixins' as*;
+
+.comics-card {
+    width: calc(100% / 6 - 20px);
+    margin: 10px;
+
+    .thumb {
+        img {
+            width: 100%;
+        }
+    }
+
+    .comic-title {
+        margin: 10px 0;
+        color: $white;
+    }
+}
+</style>
